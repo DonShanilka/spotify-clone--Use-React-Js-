0f4644ec4c0ae6974fd72bfaac28bd4b335dca32
Shanilka <shanilka799@gmail.com>
2024-07-28 00:23:59 +0530

    update app jsx and navbar jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './index.css'
 import { Sidebar } from './components/Sidebar'
 import Player from './components/Player'
+import Display from './components/Display'
 
 const App = () => {
   return (
     <div className="h-screen bg-black">
       <div className='h-[90%] flext'>
         <Sidebar />
+        <Display />
       </div>
       <Player />
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,8 @@ const Navbar = () => {
         <>
             <div className='w-full flex justify-between items-center font-semibold'>
                 <div className='flex items-center gap-2'>
-                    <img className='w-8 bg-black p-2 rounded-2xl' src={assets.arrow_left} alt="" />
+                    <img className='w-8 bg-black p-2 rounded-2xl cursor-pointer' src={assets.arrow_left} alt="" />
+                    <img className='w-8 bg-black p-2 rounded-2xl cursor-pointer' src={assets.arrow_right} alt="" />
                 </div>
             </div>
         </>
