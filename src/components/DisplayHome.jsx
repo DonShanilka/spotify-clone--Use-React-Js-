3e011e308457d27d04aeff35a14e3e94b5200fd8
Shanilka <shanilka799@gmail.com>
2024-07-29 12:56:00 +0530

    DisplayHome componnent div update song list

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Navbar from './Navbar'
-import albumData from '../assets/assets'
+import { albumsData } from '../assets/assets'
 import AlbumItem from './AlbumItem'
 
 const DisplayHome = () => {
     return (
         <>
             <Navbar />
-            <div className='my-5 font-bold text-2xl'>
-                {albumData.map((item, index) => (<AlbumItem />))}
+            <div className='mb-4 '>
+                <h1 className='my-5 font-bold text-2xl'>featured Charts</h1>
+                <div className='flex overflow-auto'>
+                    {albumsData.map((item, index) => (<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
+                </div>
             </div>
         </>
     )
