13d8a9fc1d4cbd0ecd3e618f9b9da208a8df9c90
Shanilka <shanilka799@gmail.com>
2024-07-29 13:35:43 +0530

    update songitem jsx and display home jsx

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Navbar from './Navbar'
-import { albumsData } from '../assets/assets'
+import { albumsData, songsData } from '../assets/assets'
 import AlbumItem from './AlbumItem'
-import { songsData } from '../assets/assets'
+import SongItem from './SongItem'
 
 const DisplayHome = () => {
     return (
@@ -18,7 +18,7 @@ const DisplayHome = () => {
             <div className='mb-4 '>
                 <h1 className='my-5 font-bold text-2xl'> Today's biggest hits</h1>
                 <div className='flex overflow-auto'>
-                    {songsData}
+                    {songsData.map((item, index) => (<SongItem key={index} name={ } />))}
                 </div>
             </div>
         </>
