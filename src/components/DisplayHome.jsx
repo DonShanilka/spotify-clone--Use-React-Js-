b92e4d2f19c4037510e01b13d0713e737e05dfb5
Shanilka <shanilka799@gmail.com>
2024-07-29 12:23:50 +0530

    album jsx file aded

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from './Navbar'
+import albumData from '../assets/assets'
+import AlbumItem from './AlbumItem'
 
 const DisplayHome = () => {
     return (
         <>
             <Navbar />
+            <div className='my-5 font-bold text-2xl'>
+                {albumData.map((item, index) => (<AlbumItem />))}
+            </div>
         </>
     )
 }
