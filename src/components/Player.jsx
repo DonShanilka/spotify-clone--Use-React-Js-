09ce55b0995f476d91a4f30f4ced80e0a6692a63
Shanilka <shanilka799@gmail.com>
2024-07-29 21:24:38 +0530

    update

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets, songsData } from '../assets/assets'
+import { PlayerContext } from '../context/PlayerContext'
 
 
 const Player = () => {
+
+    const { seekBar, seekBg, playStatus, play, pause } = useContext(PlayerContext);
+
     return (
         <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
             <div className='hidden lg:flex items-center gap-4'>
@@ -13,17 +17,20 @@ const Player = () => {
                 </div>
             </div>
             <div className='flex flex-col items-center gap-1 m-auto'>
-                <div className='flex gap-5'>
+                <div className='flex gap-4'>
                     <img className='w-4 cursor-pointer' src={assets.shuffle_icon} alt="" />
                     <img className='w-4 cursor-pointer' src={assets.prev_icon} alt="" />
-                    <img className='w-4 cursor-pointer' src={assets.play_icon} alt="" />
+                    {playStatus
+                        ? <img onClick={pause} className='w-4 cursor-pointer' src={assets.pause_icon} alt="" />
+                        : <img onClick={play} className='w-4 cursor-pointer' src={assets.play_icon} alt="" />
+                    }
                     <img className='w-4 cursor-pointer' src={assets.next_icon} alt="" />
                     <img className='w-4 cursor-pointer' src={assets.loop_icon} alt="" />
                 </div>
                 <div className='flex item-center gap-5'>
                     <p>1:06</p>
-                    <div className='h-1 w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
-                        <hr className='h-1 border-none w-0 bg-green-800 rounded-full' />
+                    <div ref={seekBg} className='h-1 w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
+                        <hr ref={seekBar} className='h-1 border-none w-0 bg-green-800 rounded-full' />
                     </div>
                     <p>3:20</p>
                 </div>
