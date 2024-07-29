03cd735384bd13114ec556bba3b3fd71a1fa4ee6
Shanilka <shanilka799@gmail.com>
2024-07-29 13:37:37 +0530

    add song list

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -18,7 +18,7 @@ const DisplayHome = () => {
             <div className='mb-4 '>
                 <h1 className='my-5 font-bold text-2xl'> Today's biggest hits</h1>
                 <div className='flex overflow-auto'>
-                    {songsData.map((item, index) => (<SongItem key={index} name={ } />))}
+                    {songsData.map((item, index) => (<SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
                 </div>
             </div>
         </>
