3f70ad835073f31f68333f5e52b351d406c00160
Shanilka <shanilka799@gmail.com>
2024-07-29 19:29:54 +0530

    add PlayerContext.jsx

---
+++ src/context/PlayerContext.jsx
@@ -0,0 +1,19 @@
+import { createContext } from "react";
+
+export const PlayerContext = createContext();
+
+const PlayerContextProvider = (props) => {
+
+    const contextValue = {
+
+    }
+
+    return (
+        <PlayerContext.Provider value={contextValue}>
+            {props.childern}
+        </PlayerContext.Provider>
+    )
+
+}
+
+export default PlayerContextProvider;
