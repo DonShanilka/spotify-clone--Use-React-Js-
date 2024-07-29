d4e993115dd531cf44b776a48a276230fbf36e9f
Shanilka <shanilka799@gmail.com>
2024-07-29 18:31:45 +0530

    add song card navigate song list

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { useParams } from 'react-router-dom'
-import { albumsData, assets } from '../assets/assets';
+import { albumsData, assets, songsData } from '../assets/assets';
 
 const DisplayAlbum = () => {
 
@@ -18,10 +18,35 @@ const DisplayAlbum = () => {
                     <h2 className='text-5xl font-bold mb-4 md:text-7xl'>{albumData.name}</h2>
                     <h4>{albumData.desc}</h4>
                     <p>
-                        <img src={assets.spotify_logo} alt="" />
+                        <img className='inline-block w-5' src={assets.spotify_logo} alt="" />
+                        <b>Spotify</b>
+                        • 1,323,154 likes
+                        • <b>50 songs, </b>
+                        about 2 hr 30 min
                     </p>
                 </div>
             </div>
+            <div className='grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text[#a7a7a7]'>
+                <p><b className='mr-4'>#</b>Title</p>
+                <p>Album</p>
+                <p className='hidden sm:block'>Date Added</p>
+                <img className='m-auto w-4' src={assets.clock_icon} alt="" />
+            </div>
+            <hr />
+            {
+                songsData.map((items, indexs) => (
+                    <div key={indexs} className='grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer'>
+                        <p className='text-white'>
+                            <b className='mr-4 text-[#a7a7a7]'>{indexs + 1}</b>
+                            <img className='inline w-10 mr-5' src={items.image} alt="" />
+                            {items.name}
+                        </p>
+                        <p className='text-[15px]'>{albumData.name}</p>
+                        <p className='text-[15px] hidden sm:block'>5 days ago</p>
+                        <p className='text-[15px] text-center'>{items.duration}</p>
+                    </div>
+                ))
+            }
         </>
     )
 }
