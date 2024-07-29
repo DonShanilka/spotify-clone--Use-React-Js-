b6033f0d4447181149905fa9723d45c530ec2b1e
Shanilka <shanilka799@gmail.com>
2024-07-29 12:59:38 +0530

    add song data div in display \home page

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Navbar'
 import { albumsData } from '../assets/assets'
 import AlbumItem from './AlbumItem'
+import { songsData } from '../assets/assets'
 
 const DisplayHome = () => {
     return (
@@ -11,6 +12,13 @@ const DisplayHome = () => {
                 <h1 className='my-5 font-bold text-2xl'>featured Charts</h1>
                 <div className='flex overflow-auto'>
                     {albumsData.map((item, index) => (<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
+                </div>
+            </div>
+
+            <div className='mb-4 '>
+                <h1 className='my-5 font-bold text-2xl'> Today's biggest hits</h1>
+                <div className='flex overflow-auto'>
+                    {songsData}
                 </div>
             </div>
         </>
